14897c4e1ba8f8f075c55d120c03e51b37f9d281
sunique <suntao@tongji.edu.cn>
2018-12-15 22:35:06 +0800

    little fix

--- deep_raod_finder/settings.py
+++ deep_raod_finder/settings.py
@@ -54,8 +54,7 @@ ROOT_URLCONF = 'deep_raod_finder.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')]
-        ,
+        'DIRS': ['frontend/dist'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -119,3 +118,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "frontend/dist/static"),
+]
--- deep_raod_finder/urls.py
+++ deep_raod_finder/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from deep_raod_finder.index import views as index_view
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path(r'', index_view.index_page)
 ]
